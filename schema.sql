3dc7cf4ac3aab4f3601266178fb45e61afe591e9
@ojrdev <contato@ojrdev.com>
2020-11-29 00:39:50 -0300

    First working version

---
+++ schema.sql
@@ -0,0 +1,11 @@
+-- shafs example schema
+-- hardcoded in shafs.c
+
+CREATE TABLE shafs(
+    filepath VARCHAR(4096) PRIMARY KEY, -- sqlite doesnt care
+    filehash CHAR(32),
+    filesize INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE INDEX by_filehash ON shafs(filehash);
+CREATE INDEX by_filesize ON shafs(filesize);
